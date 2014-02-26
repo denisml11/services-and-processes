64a70c1c434428e93b6cb5fa279a625411a17d7a
Denis <denis@ubuntu.(none)>
2014-02-26 23:48:26 +0100

    Creo que con spawn ha de correrse otro programa, por lo tanto la parte de ficheros alomejor ponerlo en otro cpp, correjir

--- solution/linux-c/spawnChild.cpp
+++ solution/linux-c/spawnChild.cpp
@@ -3,27 +3,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int spawn ()
-{
+int spawn (){
+
+
 	pid_t child_pid;
+
 	FILE * pfichero;
 	char * alumnos[10]= {"pepe", "jose", "denis", "maria", "ana", "davida", "ole", "ola", "hola", "aloh"};
-	char * alumnos2[10]= {"sgf", "jsd", "denissf", "mariasfs", "anasss", "davidasss", "oless", "olasss", "holasss", "alohss"};
+	char * alumnos2[10]= {"bien", "mal", "si", "no", "delante", "detras", "derecha", "izquierda", "blanco", "negro"};
+	//	char * a1[10];
+
 	child_pid = fork ();
+
 	if (child_pid != 0){
 		pfichero = fopen("alumnos.txt", "w");
 		for (int i = 0; i < 10; i++)
 			fprintf(pfichero, "%s ", alumnos[i]);
-	}else {
+	}else{
 		pfichero = fopen("alumnos.txt", "a+");
 		for (int e = 0; e < 10; e++)
 			fprintf(pfichero, "%s ", alumnos2[e]);
 
 	}
+
+
+	//	for (int x = 0; x < 10; x++){
+	//		fscanf(pfichero, " %s", &a1[x]);
+	//		printf("%s ", a1[x]);
+	//	}
+	fclose(pfichero);
 }
-int main ()
-{
+
+int main (){
+
 	spawn();
-	printf ("done with main program\n");
-	return 0;
+	printf ("OK\n");
+	return EXIT_SUCCESS;
 }
